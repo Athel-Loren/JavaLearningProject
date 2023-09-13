e7f9b8d87c1caf2693a3f20bb1427af28fad5bf2
alexa <alexandrov.dumitru.98@gmail.com>
2023-09-13 20:08:29 +0300

    Defined Git WorkFlow for future development strategy

---
+++ src/main/java/md/tekwillacademy/gitworkflow/GitWorkFlow.java
@@ -0,0 +1,38 @@
+package md.tekwillacademy.gitworkflow;
+
+public class GitWorkFlow {
+    public static void main(String[] args) {
+
+        String step1 = "Execute git stash in the console";
+        String step2 ="Perform git checkout master - to move to the local master branch";
+        String step3 ="Execute git fetch command -";
+        String step4 = "Step 4";
+        String step5 = "Step 4";
+        String step6 = "Step 4";
+        String step7 = "Step 4";
+        String step8 = "Execute git commit";
+        String step9 = "Execute git push";
+        String step10 = "Create pull request from origin/...";
+        String step11 = "Add pull request details like name, description and reviewers";
+        String step12 = "If we collect at least one approval...";
+        String step13 = "Don't forget that locally...";
+
+        System.out.println(step1);
+        System.out.println(step2);
+        System.out.println(step3);
+        System.out.println(step4);
+        System.out.println(step5);
+        System.out.println(step6);
+        System.out.println(step7);
+        System.out.println(step8);
+        System.out.println(step9);
+        System.out.println(step10);
+        System.out.println(step11);
+        System.out.println(step12);
+        System.out.println(step13);
+
+
+
+
+    }
+}
