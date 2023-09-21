c8da77d9280b62103f377f8061cc429e48c5b3ec
alexa <alexandrov.dumitru.98@gmail.com>
2023-09-21 20:55:44 +0300

    Implemented the AutoServiceTask.

---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Person.java
@@ -0,0 +1,12 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Person {
+    public  String phoneNumber;
+    public Person(String enteredThePhoneNumberAtInitializationStage){
+        phoneNumber=enteredThePhoneNumberAtInitializationStage;
+    }
+
+    public Person(){
+        phoneNumber ="100000000000000000000";
+    }
+}
